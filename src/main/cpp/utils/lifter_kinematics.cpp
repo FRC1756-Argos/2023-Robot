3b205177e52e1d6d4cfefa3e82b91331308784fb
dkt01 <dkt01@users.noreply.github.com>
2023-01-22 17:50:32 -0600

    Clean up math a bit

--- src/main/cpp/utils/lifter_kinematics.cpp
+++ src/main/cpp/utils/lifter_kinematics.cpp
@@ -13,18 +13,19 @@
 
 #include "units/math.h"
 
+auto Squared(auto val) {
+  return units::math::pow<2, decltype(val)>(val);
+}
+
 LifterKinematics::LifterKinematics() = default;
 
 LifterState LifterKinematics::GetJoints(frc::Translation2d pose, frc::Transform2d effectorOffset) {
   units::meter_t poseMagnitude = pose.Distance(frc::Translation2d(0_m, 0_m));
 
-  auto arm_len = units::math::sqrt(units::math::pow<2, units::meter_t>(poseMagnitude) -
-                                   units::math::pow<2, units::meter_t>(units::math::abs(effectorOffset.Y())));
-  auto shoulderAngle =
-      units::math::atan2<units::length::meter_t, units::length::meter_t>(pose.Y(), pose.X()) -
-      units::math::atan(units::math::sqrt(units::math::pow<2, units::meter_t>(poseMagnitude) -
-                                          units::math::pow<2, units::meter_t>(units::math::abs(effectorOffset.Y()))) /
-                        units::math::abs(effectorOffset.Y()));
+  auto arm_len = units::math::sqrt(Squared(poseMagnitude) - Squared(effectorOffset.Y()));
+  auto shoulderAngle = units::math::atan2(pose.Y(), pose.X()) -
+                       units::math::atan(units::math::sqrt(Squared(poseMagnitude) - Squared(effectorOffset.Y())) /
+                                         units::math::abs(effectorOffset.Y()));
 
   LifterState lift_state{arm_len, shoulderAngle};
 
@@ -33,8 +34,7 @@ LifterState LifterKinematics::GetJoints(frc::Translation2d pose, frc::Transform2
 
 frc::Translation2d LifterKinematics::GetPose(LifterState state, frc::Transform2d effectorOffset) {
   units::radian_t rotation =
-      (units::make_unit<units::radian_t>(std::numbers::pi_v<double> * 2) -
-       units::math::atan2<units::length::meter_t, units::length::meter_t>(effectorOffset.Y(), state.armLen)) +
+      (units::radian_t(std::numbers::pi_v<double> * 2) - units::math::atan2(effectorOffset.Y(), state.armLen)) +
       state.shoulderAngle + units::math::atan(state.armLen / units::math::abs(effectorOffset.Y()));
 
   frc::Translation2d initPosition{effectorOffset.X(), effectorOffset.Y()};
