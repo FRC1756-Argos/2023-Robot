526643e584c9faf896601088b68a10e0b5308064
Jacob S <jsimeone0105@gmail.com>
2023-01-25 21:37:25 -0600

    Fix weird conflict of motor type with main

--- src/main/include/subsystems/intake_subsystem.h
+++ src/main/include/subsystems/intake_subsystem.h
@@ -27,5 +27,5 @@ class IntakeSubsystem : public frc2::SubsystemBase {
  private:
   // Components (e.g. motor controllers and sensors) should generally be
   // declared private and exposed only through public methods.
-  TalonFX m_intakeMotor;
+  WPI_TalonSRX m_intakeMotor;
 };
