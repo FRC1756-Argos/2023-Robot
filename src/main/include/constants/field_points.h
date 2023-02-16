f95d6afe2524a1b0969525fd5e9e8ef0cfb50e54
GHatten <97795242+GHatten@users.noreply.github.com>
2023-02-15 19:33:29 -0600

    Noted the heights, depths, and widths scoring nodes and substations

--- src/main/include/constants/field_points.h
+++ src/main/include/constants/field_points.h
@@ -11,4 +11,29 @@
 
 #include "measure_up.h"
 
-namespace field_points {}  // namespace field_points
+namespace field_points {
+  constexpr auto highConeNodeHeight = 46_in;
+  constexpr auto highConeNodeDepth = 39.75_in;
+  constexpr auto highCubeNodeHeight = 35.5_in;
+  constexpr auto highCubeNodeDepth = 39.75_in;
+  // high cone and cube node heights from floor and depths
+  constexpr auto middleConeNodeHeight = 34_in;
+  constexpr auto middleConeNodeDepth = 22.75_in;
+  constexpr auto middleCubeNodeHeight = 23.5_in;
+  constexpr auto middleCubeNodeDepth = 22.75_in;
+  // middle cone and cube node heights from floor and depths
+  constexpr auto lowConeNodeHeight = 0_in;
+  constexpr auto lowConeNodeDepth = 8_in;
+  constexpr auto lowCubeNodeHeight = 0_in;
+  constexpr auto lowCubeNodeDepth = 8_in;
+  // low cone and cube node heights from floor and depths
+  constexpr auto doubleSubstationHeight = 37.375_in;
+  constexpr auto doubleSubstationDepth = 13_in;
+  constexpr auto doubleSubstationSliderWidth = 14_in;
+  // double human player substation height from floor, depth, and width
+  constexpr auto singleSubstationHeightTop = 45.125_in;
+  constexpr auto singleSubstationHeightBottom = 27.125_in;
+  constexpr auto singleSubstationWidth = 22.75_in;
+  // single human player substation portal height from floor to bottom, floor to top, and width
+
+}  // namespace field_points
