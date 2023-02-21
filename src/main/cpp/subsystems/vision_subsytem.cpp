1901fbd3975010cb51b28d997904ae3b2883808f
Nikdogge <nikdogge@users.noreply.github.com>
2023-02-20 20:23:06 -0600

    Added vision subsystem with camera interface

--- src/main/cpp/subsystems/vision_subsytem.cpp
+++ src/main/cpp/subsystems/vision_subsytem.cpp
@@ -8,3 +8,24 @@ VisionSubsytem::vision_subsytem() = default;
 
 // This method will be called once per scheduler run
 void vision_subsytem::Periodic() {}
+
+// LIMELIGHT TARGET MEMBER FUNCTIONS ===============================================================
+
+LimelightTarget::tValues LimelightTarget::GetTarget() {
+  std::shared_ptr<nt::NetworkTable> table = nt::NetworkTableInstance::GetDefault().GetTable("limelight");
+
+  m_yaw = units::make_unit<units::degree_t>(table->GetNumber("tx", 0.0));
+  m_pitch = units::make_unit<units::degree_t>(table->GetNumber("ty", 0.0));
+  m_targetPose = (table->GetNumberArray("botpose",std::vector<double>(6));
+  m_transform3d = (table->GetNumberArray("targetpose_robotspace",std::vector<double>(6));
+  m_targetId = int{(table->GetNumber("tid", 0.0))};
+  m_hasTargets = (table->GetNumber("tv", 0) == 1);
+  m_pipelineLatency = units::millisecond_t{table->GetNumber("tl", 0.0)};
+
+  tValues targetValues{m_pitch, m_yaw, m_targetPose, m_transform3d, m_targetId, totalLatency};
+  return targetValues;
+}
+
+bool LimelightTarget::HasTarget() {
+  return m_hasTargets;
+}
--- src/main/include/subsystems/vision_subsytem.h
+++ src/main/include/subsystems/vision_subsytem.h
@@ -4,11 +4,102 @@
 
 #pragma once
 
+#include <frc/geometry/Pose3d.h>
+#include <frc/geometry/Transform3d.h>
 #include <frc2/command/SubsystemBase.h>
+
+#include "argos_lib/config/config_types.h"
+#include "networktables/NetworkTable.h"
+#include "networktables/NetworkTableEntry.h"
+#include "networktables/NetworkTableInstance.h"
+#include "networktables/NetworkTableValue.h"
+
+class LimelightTarget {
+ private:
+  frc::Pose3d m_targetPose;                ///< 3d pose of target in view
+  frc::Transform3d m_targetToRobot;        ///< 3d transform of target in view in robot space
+  int m_targetId;                          ///< April tag id of the target in view
+  units::degree_t m_pitch;                 ///< Pitch of target relative to camera -20.5 to 20.5 degrees
+  units::degree_t m_yaw;                   ///< Yaw of target relative to camera -27 to 27 degrees
+  bool m_hasTargets;                       ///< True if the camera has a target it can read
+  units::millisecond_t m_pipelineLatency;  ///< Pipeline latency contribution
+  constexpr static units::millisecond_t m_miscLatency{11_ms};  ///< Any extra latency to account for
+
+ public:
+  LimelightTarget() = default;
+
+  /**
+   * @brief Wraps members of LimelightTarget for use elsewhere
+   *
+   */
+  struct tValues {
+    units::degree_t pitch;                    ///< See LimelightTarget::m_pitch
+    units::degree_t yaw;                      ///< See LimelightTarget::m_yaw
+    frc::Pose3d targetPose;                   ///< See LimelightTarget::m_targetPose
+    frc::Transform3d targetToRobotTransform;  ///< See LimelightTarget::m_targetToRobot
+    int targetId;                             ///< See LimelightTarget::m_targetId
+    units::millisecond_t totalLatency;        ///< See LimelightTarget::m_pipelineLatency
+  };
+
+  /**
+   * @brief Get the values of the camera's current target
+   *
+   * @return tValues
+   */
+  tValues GetTarget();
+
+  /**
+   * @brief Does the camera see a target?
+   *
+   * @return true - The camera does see a target
+   * @return false - The camera does not see a target
+   */
+  bool HasTarget();
+};
+
+/**
+ * @brief Provides methods for interacting with the camera on a high level
+ *
+ */
+class CameraInterface {
+ public:
+  CameraInterface();
+
+  LimelightTarget m_target;  ///< object that holds the current target seen by the camera
+
+  /**
+   * @brief Get the closest target the camera can see CAN RETURN NONE
+   *
+   * @return std::optional<photonlib::PhotonTrackedTarget>
+   */
+  std::optional<LimelightTarget> GetClosestTarget();
+
+  /**
+   * @brief Turns the camera's driver mode on and off
+   *
+   * @param mode True is drive control. False is no drive control
+   */
+  void SetDriverMode(bool mode);
+};
 
 class vision_subsytem : public frc2::SubsystemBase {
  public:
   vision_subsytem();
+
+  /**
+   * @brief Get the offset to the center of the target
+   *
+   * @param target Target to aim toward
+   * @return Desired offset.
+   */
+  std::optional<units::degree_t> GetOffsetToTarget(LimelightTarget::tValues target);
+
+  /**
+   * @brief Get the pitch and yaw of target
+   *
+   * @return LimelightTarget::tValues
+   */
+  LimelightTarget::tValues GetCameraTargetValues();
 
   /**
    * Will be called periodically whenever the CommandScheduler runs.
@@ -18,4 +109,8 @@ class vision_subsytem : public frc2::SubsystemBase {
  private:
   // Components (e.g. motor controllers and sensors) should generally be
   // declared private and exposed only through public methods.
+  CameraInterface m_cameraInterface;  ///< Interface to limelight camera
+
+  argos_lib::RobotInstance
+      m_instance;  ///< Contains either the competition bot or practice bot. Differentiates between the two
 };
