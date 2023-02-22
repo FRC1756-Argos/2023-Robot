f1c9fb6c10113b1418fbe7ff7147e9903b1a95d5
dkt01 <dkt01@users.noreply.github.com>
2023-02-21 20:55:56 -0600

    Set default fire pattern

--- src/main/cpp/subsystems/simple_led_subsystem.cpp
+++ src/main/cpp/subsystems/simple_led_subsystem.cpp
@@ -11,7 +11,9 @@
 
 SimpleLedSubsystem::SimpleLedSubsystem(argos_lib::RobotInstance instance)
     : m_CANdle{GetCANAddr(address::comp_bot::led::CANdle, address::practice_bot::led::CANdle, instance),
-               std::string(GetCANBus(address::comp_bot::led::CANdle, address::practice_bot::led::CANdle, instance))} {}
+               std::string(GetCANBus(address::comp_bot::led::CANdle, address::practice_bot::led::CANdle, instance))} {
+  FireEverywhere();
+}
 // This method will be called once per scheduler run
 void SimpleLedSubsystem::Periodic() {}
 
@@ -22,21 +24,21 @@ void SimpleLedSubsystem::SetBackLeftSolidColor(frc::Color color) {
 
 void SimpleLedSubsystem::FireEverywhere() {
   auto fireAnimationBL =
-      ctre::phoenix::led::FireAnimation(1, 1, length_backLeft, 1, 0.2, inverted_backLeft, startIndex_backLeft);
+      ctre::phoenix::led::FireAnimation(0.5, 0.7, length_backLeft, 1, 0.2, inverted_backLeft, startIndex_backLeft);
   m_CANdle.Animate(fireAnimationBL, 0);
   auto fireAnimationBR =
-      ctre::phoenix::led::FireAnimation(1, 1, length_backRight, 1, 0.2, inverted_backRight, startIndex_backRight);
+      ctre::phoenix::led::FireAnimation(0.5, 0.7, length_backRight, 1, 0.2, inverted_backRight, startIndex_backRight);
   m_CANdle.Animate(fireAnimationBR, 1);
   auto fireAnimationSB =
-      ctre::phoenix::led::FireAnimation(1, 1, length_sideBack, 1, 0.2, inverted_sideBack, startIndex_sideBack);
+      ctre::phoenix::led::FireAnimation(0.5, 0.7, length_sideBack, 1, 0.2, inverted_sideBack, startIndex_sideBack);
   m_CANdle.Animate(fireAnimationSB, 2);
   auto fireAnimationSF =
-      ctre::phoenix::led::FireAnimation(1, 1, length_sideFront, 1, 0.2, inverted_sideFront, startIndex_sideFront);
+      ctre::phoenix::led::FireAnimation(0.5, 0.7, length_sideFront, 1, 0.2, inverted_sideFront, startIndex_sideFront);
   m_CANdle.Animate(fireAnimationSF, 3);
   auto fireAnimationFL =
-      ctre::phoenix::led::FireAnimation(1, 1, length_frontLeft, 1, 0.2, inverted_frontLeft, startIndex_frontLeft);
+      ctre::phoenix::led::FireAnimation(0.5, 0.7, length_frontLeft, 1, 0.2, inverted_frontLeft, startIndex_frontLeft);
   m_CANdle.Animate(fireAnimationFL, 4);
-  auto fireAnimationFR =
-      ctre::phoenix::led::FireAnimation(1, 1, length_frontRight, 1, 0.2, inverted_frontRight, startIndex_frontRight);
+  auto fireAnimationFR = ctre::phoenix::led::FireAnimation(
+      0.5, 0.7, length_frontRight, 1, 0.2, inverted_frontRight, startIndex_frontRight);
   m_CANdle.Animate(fireAnimationFR, 5);
 }
