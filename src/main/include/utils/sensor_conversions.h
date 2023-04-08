0324713706c27705752ab4cfc326a5b9c58a0ea9
Jacob S <jsimeone0105@gmail.com>
2023-04-08 11:31:35 -0500

    Alter bash ration to 5:1

--- src/main/include/utils/sensor_conversions.h
+++ src/main/include/utils/sensor_conversions.h
@@ -139,7 +139,7 @@ namespace sensor_conversions {
   }    // namespace lifter
   namespace bashguard {
     constexpr double sensorToMotorRevolution = 1.0 / 2048;
-    constexpr double gearboxReduction = 1.0 / 12;
+    constexpr double gearboxReduction = 1.0 / 5;
     constexpr double driveSprocketTeeth = 15.0;
     constexpr double extensionInchesPerTooth = 0.375 / 1;
     constexpr units::inch_t ToExtension(const double sensorUnit) {
