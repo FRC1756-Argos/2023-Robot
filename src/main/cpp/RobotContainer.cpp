a062d91c6bea443909d59967bbd3acd180a67966
Teja M <kanakamteja@gmail.com>
2023-04-09 20:41:11 -0500

    update

--- src/main/cpp/RobotContainer.cpp
+++ src/main/cpp/RobotContainer.cpp
@@ -69,6 +69,7 @@ RobotContainer::RobotContainer()
     , m_autoPlaceExit{m_swerveDrive, m_bash, m_lifter, m_ledSubSystem, m_intake}
     , m_autoCableProtector2Gp{m_swerveDrive, m_bash, m_lifter, m_intake, m_ledSubSystem}
     , m_autoCableProtector3Gp{m_swerveDrive, m_bash, m_lifter, m_intake, m_ledSubSystem}
+    , m_autoCableProtector3GpOuiOui{m_swerveDrive, m_bash, m_lifter, m_intake, m_ledSubSystem, m_ouiOuiPlacerSubsystem}
     , m_autoCablePlaceExit{m_swerveDrive, m_bash, m_lifter, m_ledSubSystem, m_intake}
     , m_autoScorePickupBalanceCone{m_swerveDrive, m_bash, m_lifter, m_intake, m_ledSubSystem}
     , m_autoSelector{{&m_autoNothing,
@@ -81,6 +82,7 @@ RobotContainer::RobotContainer()
                       &m_autoConeCubeScore,
                       &m_autoCableProtector2Gp,
                       &m_autoCableProtector3Gp,
+                      &m_autoCableProtector3GpOuiOui,
                       &m_auto3gp,
                       &m_autoScorePickupBalanceCone,
                       &m_autoOnlyBalance},
