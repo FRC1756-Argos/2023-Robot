8a704b1e1929278486b6d2b0ece32b5572224438
dkt01 <dkt01@users.noreply.github.com>
2023-04-20 08:44:01 -0500

    Lower cone pickup a smidge

--- src/main/include/constants/scoring_positions.h
+++ src/main/include/constants/scoring_positions.h
@@ -47,7 +47,7 @@ namespace scoring_positions {
     constexpr static SetpointPosition cubeHigh(frc::Translation2d(48.5_in, 49_in), BashGuardPosition::Retracted);
     constexpr static SetpointPosition cubeHigh_wristInverted(frc::Translation2d(48.5_in, 49_in),
                                                              BashGuardPosition::Retracted);
-    constexpr static SetpointPosition coneIntake(frc::Translation2d(31.5_in, 13.5_in), BashGuardPosition::Deployed);
+    constexpr static SetpointPosition coneIntake(frc::Translation2d(31.5_in, 13.25_in), BashGuardPosition::Deployed);
     constexpr static SetpointPosition cubeIntake(frc::Translation2d(31.5_in, 14.75_in), BashGuardPosition::Deployed);
     constexpr static SetpointPosition stow(frc::Translation2d(10.5_in, 19.5_in), BashGuardPosition::Retracted);
   }  // namespace lifter_extension_end
